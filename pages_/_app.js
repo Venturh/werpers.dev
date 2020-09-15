561d1156fdf3c17af9c2afe7bb6c9cf1391a7c9b
Venturh <venturh94@gmail.com>
2020-09-15 16:36:12 +0200

    moved favicons from document to app

--- pages_/_app.js
+++ pages_/_app.js
@@ -2,11 +2,38 @@ import { ThemeProvider } from "styled-components";
 import { DefaultSeo } from "next-seo";
 import SEO from "next-seo.config";
 import { GlobalStyles, theme } from "styles";
-import { Head } from "next/document";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#161821"
+        />
+        <meta name="msapplication-TileColor" content="#161821" />
+        <meta name="theme-color" content="#161821"></meta>
+      </Head>
       <DefaultSeo {...SEO} />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
--- pages_/_document.js
+++ pages_/_document.js
@@ -1,55 +1,30 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
-  }
-
-  render() {
-    return (
-      <Html lang="de">
-        <Head>
-          {this.props.styleTags}
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicons/safari-pinned-tab.svg"
-            color="#161821"
-          />
-          <meta name="msapplication-TileColor" content="#161821" />
-          <meta name="theme-color" content="#161821"></meta>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
