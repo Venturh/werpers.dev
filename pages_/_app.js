abd84195a7b2bb1552748ea655701a0d6c61c171
Venturh <venturh94@gmail.com>
2020-09-15 16:20:03 +0200

    refactored favoicons, adjusted _document to show favicons

--- pages_/_app.js
+++ pages_/_app.js
@@ -9,33 +9,6 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <DefaultSeo {...SEO} />
       <ThemeProvider theme={theme}>
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicons/safari-pinned-tab.svg"
-            color="#161821"
-          />
-          <meta name="msapplication-TileColor" content="#161821" />
-          <meta name="theme-color" content="#161821"></meta>
-        </Head>
         <GlobalStyles />
         <Component {...pageProps} />
       </ThemeProvider>
