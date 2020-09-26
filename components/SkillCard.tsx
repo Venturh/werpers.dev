2d623f552335a7ad9e9016205fc15b130b35b6ed
Venturh <venturh94@gmail.com>
2020-09-26 22:37:23 +0200

    adjusted skills cards

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -1,97 +1,51 @@
-import { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import Icon from "./Icon";
-import { Caption, Subtitle, Text } from "./Typography";
+import { Caption, Text } from "./Typography";
 
-import { ArrowDownS, ArrowUpS } from "icons";
 import { card3 } from "styles";
-import Button from "./Button";
-import useResizeEffect from "hooks/useResizeEffect";
 
-type SkillCardProps = {
-  toggle: boolean;
-};
-
-const SkillCard = ({ title, skills }) => {
-  const [open, setOpen] = useState(false);
-  const [overflow, setOverflow] = useState(false);
-  const skillsRef = useRef<HTMLElement | null>(null);
-
-  const checkOverflow = useCallback(() => {
-    if (!skillsRef.current) return;
-    const { scrollHeight } = skillsRef.current;
-    setOverflow(scrollHeight > 47);
-  }, [skillsRef, open]);
-
-  useResizeEffect(checkOverflow, [skillsRef]);
-
-  return (
-    <Card toggle={open}>
-      <Type style={{ marginLeft: "0.5em" }}>{title}</Type>
-      <Skills toggle={open}>
-        {skills.map((skill) => (
-          <Skill
-            ref={(ref) => {
-              if (ref) {
-                skillsRef.current = ref;
-              }
-            }}
-            key={skill.name}
-          >
-            <Icon color="primary" size="1.5em" path={skill.icon} />
-
-            <Caption style={{ paddingTop: "0.25em" }}>{skill.name}</Caption>
-          </Skill>
-        ))}
-      </Skills>
-      {!overflow ? (
-        <Button
-          onClick={() => setOpen(!open)}
-          leftIcon={open ? ArrowUpS : ArrowDownS}
-          color="bodyContrast"
-          iconSize="2em"
-        />
-      ) : (
-        <Place />
-      )}
-    </Card>
-  );
-};
+const SkillCard = ({ title, skills }) => (
+  <Card>
+    <Text>{title}</Text>
+    <Skills>
+      {skills.map((skill: { icon: string; name: string }) => (
+        <Skill key={skill.name}>
+          <Icon color="primary" size="1.5em" path={skill.icon} />
+          <Caption style={{ paddingTop: "0.25em" }}>{skill.name}</Caption>
+        </Skill>
+      ))}
+    </Skills>
+  </Card>
+);
 
 export default SkillCard;
 
-const Card = styled.div<SkillCardProps>`
+const Card = styled.div`
   ${card3}
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: ${(p) => (p.toggle ? "8em" : "4em")};
-  transition: height 0.15s ease-out;
+  flex-direction: column;
+  justify-content: center;
+  height: 6em;
+  padding-left: 0.5em;
+  @media (min-width: ${(props) => props.theme.breakpoints.xl}) {
+    height: 4em;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 1em;
+  }
 `;
 
-const Type = styled(Text)`
-  width: 30%;
-`;
-
-const Skills = styled.div<SkillCardProps>`
-  width: 80%;
-  height: 100%;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 3em);
-  grid-template-rows: ${(p) => (p.toggle ? "auto" : "100%")};
-  grid-auto-columns: 0;
-  justify-content: flex-end;
-  overflow-y: hidden;
+const Skills = styled.div`
+  margin-top: 0.5em;
+  display: flex;
 `;
 
 const Skill = styled.div`
+  margin: 0 0.5em;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
-
-const Place = styled.div`
-  padding: 0.5em;
 `;
