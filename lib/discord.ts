f93b014f280ae01b3c8aa38c789742f5d7fdfcb3
Venturh <venturh94@gmail.com>
2021-01-03 17:23:49 +0100

    deleted discord lib

--- lib/discord.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from 'discord.js';
-
-export default async function getClient() {
-  const client = new Client();
-  await client.login(process.env.DISCORD_BOT_TOKEN);
-  return client;
-}
