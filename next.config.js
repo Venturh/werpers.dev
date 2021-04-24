950f5d877ca7501df7f5be2a8c82caa5da1fdc44
Venturh <venturh94@gmail.com>
2021-04-24 19:11:46 +0200

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,43 @@
 const nextTranslate = require('next-translate');
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src 'none';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+];
+
 module.exports = nextTranslate({
   experimental: {
     optimizeFonts: true,
@@ -7,4 +46,14 @@ module.exports = nextTranslate({
   future: {
     webpack5: true,
   },
+  headers: () => [
+    {
+      source: '/',
+      headers: securityHeaders,
+    },
+    {
+      source: '/:path*',
+      headers: securityHeaders,
+    },
+  ],
 });
